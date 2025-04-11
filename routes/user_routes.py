ab558e0101183bc83d7b4c3ed607a5b5ecca3667
i.zhidkov <live.korpus@mail.ru>
2025-04-11 07:15:24 +0300

    добавлены запросы для администратора: получение всех пользователей, изменение роли пользователю

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -96,6 +96,97 @@ def profile():
     finally:
         session.close()
 
+#* Изменение профиля 
+@user_bp.route('/profile-change', methods=['PUT'])
+@token_required
+def profile_change():
+    session = Session()
+    try:
+        token = request.headers.get('Authorization')
+        token_data = decode_token(token)
+
+        data = request.json
+
+        user = session.query(User).filter_by(id=token_data['id']).first()
+        if user is None:
+            return jsonify({"error": "No found user"}), 400
+        
+        user.name = data['name']
+        user.email = data['email']
+        user.avatar = data['avatar']
+        user.password = data['password']
+
+        session.commit()
+        return jsonify({"data": 'Ok'}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
+#* Администратор получение всех пользователей
+@user_bp.route('/admin', methods=['GET'])
+@token_required
+def admin_get_user():
+    session = Session()
+    try:
+        token_data = decode_token(request.headers.get('Authorization'))
+        user = session.query(User).filter_by(id=token_data['id']).first()
+        if user is None:
+            return jsonify({"error": "No found user"}), 400
+        user_role = session.query(Role).filter_by(id=user.id).first()
+        if user_role.name != 'admin':
+            return jsonify({"error": "No found user"}), 400
+        response = []
+        users = session.query(User).filter(User.id != user.id).all()
+        for us in users:
+            role_us = session.query(Role).filter_by(id=us.role_id).first()
+            response.append({
+                'id': us.id,
+                'name': us.name,
+                'email': us.email,
+                'avatar': us.avatar,
+                'password': us.password,
+                'role': role_us.name
+            })
+
+        return jsonify({"data": response}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+   
+#* Администратор изменение роли пользователю
+@user_bp.route('/admin-change-role/<int:id_user>/<int:id_role>', methods=['PUT'])
+@token_required
+def admin_change_role(id_user, id_role):
+    session = Session()
+    try:
+        token_data = decode_token(request.headers.get('Authorization'))
+        user = session.query(User).filter_by(id=token_data['id']).first()
+        if user is None:
+            return jsonify({"error": "No found user"}), 400
+        user_role = session.query(Role).filter_by(id=user.id).first()
+        if user_role.name != 'admin':
+            return jsonify({"error": "No found user"}), 400
+        
+        user_get = session.query(User).filter_by(id=id_user).first()
+        if not user_get:
+            return jsonify({"error": "No found user"}), 400
+        if user_get.id == user.id:
+            return jsonify({"error": "No found user"}), 400
+
+        if user_get.role_id == 1:
+            user_get.role_id = 2
+        elif user_get.role_id == 2:
+            user_get.role_id = 1
+        
+        session.commit()
+        return jsonify({"data": 'Ok'}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
 #* Получение аватара
 @user_bp.route('/avatar', methods=['GET'])
 def get_photo():
